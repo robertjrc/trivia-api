813be9bf1f5f31006551ca5aa1cae4f8d4e2eaa1
Robert <roberto88526@gmail.com>
2025-02-13 13:10:26 -0300

    change in the impression of the overall score.

--- src/services/Player/overallScore.js
+++ src/services/Player/overallScore.js
@@ -8,9 +8,9 @@ class PlayerOverallScore {
 
         for (var player of players) {
             text += `\n*${player.name}*\n`
-            text += "╰─ "
-            text += `🔥  ${player.score} pontos\n`
-            text += `       🎯  ${((player.hits/player.moves)*100).toFixed(0)}% de certo`
+            text += "╰─⊰"
+            text += `🔥 *${player.score}* pontos\n`
+            text += `         🎯 *${((player.hits/player.moves)*100).toFixed(0)}%* de certo`
             text += "\n"
         }
 
