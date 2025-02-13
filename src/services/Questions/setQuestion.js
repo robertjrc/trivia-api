8403bd6138514fc6e4ca208a4a28b8a969e4000a
Robert <roberto88526@gmail.com>
2025-02-13 13:11:46 -0300

    a small change in the choice of the new question from + to -.

--- src/services/Questions/setQuestion.js
+++ src/services/Questions/setQuestion.js
@@ -4,10 +4,10 @@ class QuestionSet {
     }
 
     execute(data) {
-        const chosenQuestion = data.questions[Math.floor(Math.random() * (data.questions.length + 0) + 0)]
+        const chosenQuestion = data.questions[Math.floor(Math.random() * (data.questions.length - 0) + 0)]
 
-        data.current_question = chosenQuestion.question
         data.current_response = chosenQuestion.alternatives[0]
+        data.current_question = chosenQuestion.question
         data.current_alternatives = this._alternativesGenerate.execute(chosenQuestion.alternatives)
         data.current_question_id = chosenQuestion.id
         data.moves += 1
