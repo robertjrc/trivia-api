79ff068115e3952b695cf1c038f5e80e4c28b7e6
Robert <gh.robert99@gmail.com>
2025-09-19 18:18:46 -0300

    refactor(player): Set the score to 0 if the subtraction is less than or equal to 0.

--- src/application/usecases/player/playerWrongAnswerUsecase.js
+++ src/application/usecases/player/playerWrongAnswerUsecase.js
@@ -3,7 +3,7 @@ import { Result } from "../../common/result.js";
 export class PlayerWrongAnswerUsecase {
     execute(player) {
         player.errors += 1;
-        player.score -= 3;
+        player.score = (player.score - 3 <= 0) ? 0 : player.score -= 3;
         player.moves += 1;
 
         return Result.success("Player status updated successfully.", player);
