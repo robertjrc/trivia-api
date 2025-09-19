f34b8b0a3e98103804536a32e93096732df8518d
Robert <gh.robert99@gmail.com>
2025-09-19 14:23:49 -0300

    refactor(group): questions local path update.

--- src/application/usecases/group/groupQuestionsEmptyVerifyUsecase.js
+++ src/application/usecases/group/groupQuestionsEmptyVerifyUsecase.js
@@ -1,11 +1,12 @@
 import { Result } from "../../common/result.js";
 import { importJson } from "../../utils/importJson.js";
 
-const questions = await importJson("src/infrastructure/seeds/questions.json");
-
 export class GroupQuestionsEmptyVerify {
-    execute(currentQuestions) {
+    async execute(currentQuestions) {
         if (currentQuestions.length >= 1) return Result.failure("It's not empty.", null);
+
+        const questions = await importJson("../../infrastructure/seeds/questions.json")
+
         return Result.success("It's empty.", questions);
     }
 }
