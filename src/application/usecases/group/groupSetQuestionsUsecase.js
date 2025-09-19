ee0327c148ba72482cb0ec825404225ae2c9c3d9
Robert <gh.robert99@gmail.com>
2025-09-19 18:18:11 -0300

    feat(group): set message_id.

--- src/application/usecases/group/groupSetQuestionsUsecase.js
+++ src/application/usecases/group/groupSetQuestionsUsecase.js
@@ -6,6 +6,7 @@ export class GroupSetQuestionUsecase {
         const newGroup = new Group(group.id, group.name);
 
         newGroup.setCurrentInfo(group.questions);
+        newGroup.current_info.message_id = group.message_id;
 
         return Result.success("Question successfully defined.", newGroup.current_info);
     }
