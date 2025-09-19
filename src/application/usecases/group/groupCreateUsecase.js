56950641deb380dc020ed091e6f223bcb7829b3e
Robert <gh.robert99@gmail.com>
2025-09-19 14:22:33 -0300

    refactor(group): questions local path updated.

--- src/application/usecases/group/groupCreateUsecase.js
+++ src/application/usecases/group/groupCreateUsecase.js
@@ -2,15 +2,13 @@ import { Group } from "../../../domain/entities/Group.js";
 import { Result } from "../../common/result.js";
 import { importJson } from "../../utils/importJson.js";
 
-const questions = await importJson("src/infrastructure/seeds/questions.json");
-
 export class GroupCreateUsecase {
-    execute(request) {
+    async execute(request) {
         const { id, name } = request;
 
         const newGroup = new Group(id, name);
 
-        newGroup.questions = questions;
+        newGroup.questions = await importJson("../../infrastructure/seeds/questions.json");
         newGroup.setCurrentInfo(newGroup.questions);
 
         return Result.success("Group created successfully.", newGroup);
